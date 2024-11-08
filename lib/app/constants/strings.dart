d626adba14a140955eb82b46575a5e13116ca0dd
vaisakhferoke <vaisakhperumthody@gmail.com>
2024-11-08 16:43:53 +0530

    ne cha

--- lib/app/constants/strings.dart
+++ lib/app/constants/strings.dart
@@ -4,7 +4,8 @@ const String kBaseUrl = s1;
 // const String kBaseUrlForImage = "$kBaseUrl/public/assets/web";
 const bool isStaging = true;
 
-const String s1 = "http://192.168.1.101:8088/api";
+//const String s1 = "http://192.168.1.101:8088/api";
+const String s1 = "http://iitfkeralapavilion.com/api";
 
 class LocalStorageKey {
   static String token = "USER_TOKEN";
